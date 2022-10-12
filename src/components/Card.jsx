dbe9f40b9ca892c94fb7191f1ed0812a6087c706
SalamunHasan69e <salamun2600@gmail.com>
2022-10-12 20:36:17 +0600

    edited design

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,9 +9,9 @@ const Card = ({ card }) => {
     <div className="max-w-xs rounded-md shadow-md dark:bg-gray-900 dark:text-gray-100 w-4/5 mx-auto">
       <img src={logo} alt="" className="object-cover object-center w-full rounded-t-md h-72 dark:bg-gray-500" />
       <div className="flex flex-col justify-between p-6 space-y-6">
-        <div className="flex justify-around items-center">
+        <div>
           <h2 className="text-3xl font-semibold tracking-wide">{name}</h2>
-          <small>Question: {total}</small>
+          <small className='flex justify-end'>Question: {total}</small>
         </div>
         <Link to='/quiz' type="button" className="flex items-center justify-center w-full p-3 font-semibold tracking-wide rounded-md dark:bg-violet-400 dark:text-gray-900">Quiz</Link>
       </div>
