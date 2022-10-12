55886bb78b42acd84b90f219c7384f0e0f1d72b9
SalamunHasan69e <salamun2600@gmail.com>
2022-10-12 15:10:32 +0600

    modified navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,31 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#90a5f9",
+
+          "secondary": "#f4b74e",
+
+          "accent": "#8bb6ef",
+
+          "neutral": "#261B27",
+
+          "base-100": "#EDEDEE",
+
+          "info": "#58C0DF",
+
+          "success": "#6EE7D1",
+
+          "warning": "#ED8E07",
+
+          "error": "#F33F69",
+        },
+      },
+    ],
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
